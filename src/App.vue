50566fa70b37cbf36210b37dfba74fe0436fb42d
ThisNautilus <zziaocean@foxmail.com>
2019-07-25 22:48:06 +0800

    侧边栏

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,20 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <leftNav></leftNav>
     <router-view/>
   </div>
 </template>
+
+<script>
+import leftNav from '@/components/common/leftNav.vue'
+
+export default {
+  name:'app',
+  components:{
+    leftNav
+  }
+}
+</script>
 
 <style lang="less">
 #app {
@@ -15,15 +23,5 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
